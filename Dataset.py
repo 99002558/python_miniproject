7e87e59ba4e4743d82870436af84eaa05b00ebff
99002490 <71576419+99002490@users.noreply.github.com>
2020-10-12 23:48:08 +0530

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -2,12 +2,19 @@ import csv
 class Book:
     def __init__(self,my_list):
         self.my_list=my_list    
-    def get_data(self):
-        return self.my_list       
-data_list=[]
-with open('libraryManagement.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    next(spamreader, None)
-    for row in spamreader:
-        data_list.append(row)    
-
+    def import_data(self):
+        self.data_list=[]
+        with open(self.my_list, newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            next(spamreader, None)
+            for row in spamreader:
+                self.data_list.append(row)
+        return self.data_list
+    def display(self):
+        for row in self.data_list:
+            print("Title:",row[0])
+            print("Author:",row[1])
+            print("Pages:",row[2])
+            print("Publisher:",row[3])
+            print("Id:",row[4])
+            print("Price:",row[5])
