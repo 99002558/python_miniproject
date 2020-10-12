b64c0639fe79e7ed90094064fec84c1a6c7aa62a
99002490 <71576419+99002490@users.noreply.github.com>
2020-10-12 23:49:48 +0530

    Create Calculations.py

---
+++ Calculations.py
@@ -0,0 +1,20 @@
+from Dataset import Book
+import pandas as pd
+class Calculate(Book):
+    def __init__(self,my_list):
+        super().__init__(my_list)
+    def sort_titles_in_asc(self):
+        data=self.import_data()
+        new_list=[]
+        for row in data:
+            new_list.append(row[0])
+        return sorted(new_list)
+    def sort_titles_in_desc(self):
+        data=self.import_data()
+        new_list=[]
+        for row in data:
+            new_list.append(row[0])
+        return sorted(new_list,reverse=True)
+data_set=Calculate('libraryManagement.csv')
+#print(data_set.sort_titles_in_asc())
+#print(data_set.sort_titles_in_desc())
