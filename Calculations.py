ac0fa5b540dc751e60795e0219349e2d48d3795c
99002490 <rakshith.m2@ltts.com>
2020-10-13 09:36:34 +0530

    sortascpages

--- Calculations.py
+++ Calculations.py
@@ -15,6 +15,13 @@ class Calculate(Book):
         for row in data:
             new_list.append(row[0])
         return sorted(new_list,reverse=True)
+    def sort_pages_in_asc(self):
+        data=self.import_data()
+        new_list=[]
+        for row in data:
+            new_list.append(row[2])
+        return sorted(new_list)
 data_set=Calculate('libraryManagement.csv')
 #print(data_set.sort_titles_in_asc())
 #print(data_set.sort_titles_in_desc())
+print(data_set.sort_pages_in_asc())
